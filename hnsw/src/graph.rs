10f7c3eabc3ddd7ae12cc88dde2045584f95f9b1
Gamal Abdulsalam <gamalalejandro98@gmail.com>
2024-03-16 21:35:36 +0100

    added a minimal graph structure and a test for it

--- hnsw/src/graph.rs
+++ hnsw/src/graph.rs
@@ -1,6 +1,54 @@
+use ndarray::{s, Array1};
 use std::collections::{HashMap, HashSet};
 
-struct Graph {
-    nodes: HashSet<i32>,
-    edges: HashMap<i32, (i32, i32)>,
+pub struct Graph {
+    pub nodes: HashMap<i32, HashSet<i32>>,
+    pub node_vectors: HashMap<i32, Array1<f32>>,
+    pub self_connexions: bool,
+}
+
+impl Graph {
+    pub fn add_node(&mut self, node_id: i32, vector: Array1<f32>) {
+        if !self.nodes.contains_key(&node_id) {
+            self.nodes.insert(node_id, HashSet::new());
+            self.node_vectors.insert(node_id, vector);
+        }
+    }
+
+    pub fn add_edge(&mut self, node_a: i32, node_b: i32) {
+        if (node_a == node_b) && !self.self_connexions {
+            return ();
+        }
+        let a_neighbors = self
+            .nodes
+            .get_mut(&node_a)
+            .expect("Could not get the value of node {node_a}");
+        a_neighbors.insert(node_b);
+
+        let b_neighbors = self
+            .nodes
+            .get_mut(&node_b)
+            .expect("Could not get the value of node {node_b}");
+        b_neighbors.insert(node_a);
+    }
+
+    pub fn remove_edge(&mut self, node_a: i32, node_b: i32) {
+        let a_neighbors = self
+            .nodes
+            .get_mut(&node_a)
+            .expect("Could not get neighbors of {node_a}");
+        a_neighbors.remove(&node_b);
+
+        let b_neighbors = self
+            .nodes
+            .get_mut(&node_b)
+            .expect("Could not get neighbors of {node_b}");
+        b_neighbors.remove(&node_a);
+    }
+
+    pub fn neighbors(&self, node_id: i32) -> &HashSet<i32> {
+        self.nodes
+            .get(&node_id)
+            .expect("Could not get the neighbors of {node_id}")
+    }
 }
--- hnsw/src/lib.rs
+++ hnsw/src/lib.rs
@@ -3,10 +3,51 @@ pub mod helpers;
 
 #[cfg(test)]
 mod tests {
+    use std::collections::{HashMap, HashSet};
+
     use super::*;
+    use graph::Graph;
+    use ndarray::{Array1, Array2};
+    use rand::Rng;
 
     #[test]
-    fn it_works() {
-        let a = 0;
+    fn graph_operations() {
+        let mut rng = rand::thread_rng();
+        let n = 10;
+        let mut g = Graph {
+            nodes: HashMap::new(),
+            node_vectors: HashMap::new(),
+            self_connexions: false,
+        };
+
+        let nodes = Vec::from_iter(0..n);
+        let mut node_vectors: Vec<Array1<f32>> = vec![];
+        for _ in 0..n {
+            let vector = (0..100).map(|_| rng.gen::<f32>()).collect();
+            let vect_arr = Array1::from_vec(vector);
+            node_vectors.push(vect_arr);
+        }
+
+        for (node, vector) in nodes.iter().zip(node_vectors) {
+            g.add_node(*node, vector)
+        }
+
+        let mut edges: Vec<(i32, i32)> = (0..n * 2)
+            .map(|_| (rng.gen_range(0..n), rng.gen_range(0..n)))
+            .collect();
+
+        edges.push((0, 9));
+
+        for edge in edges.iter() {
+            g.add_edge(edge.0, edge.1);
+        }
+
+        g.remove_edge(9, 0);
+
+        // run test with -- --nocapture to display
+        println!("edges: {:?}", edges);
+        for node in nodes.iter() {
+            println!("Neighbors of {node} {:?}", g.neighbors(*node));
+        }
     }
 }
