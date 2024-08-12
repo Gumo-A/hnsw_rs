a45a3297b0c1c16f5aee71a08628672fe80b03fa
Gamal Abdulsalam <gamalalejandro@gmail.com>
2024-08-12 10:01:52 +0200

    moved tests to dedicated folder

--- hnsw/src/hnsw.rs
+++ hnsw/src/hnsw.rs
@@ -1,71 +1,8 @@
 pub mod dist;
 pub mod graph;
+pub mod graph_2;
 pub mod index;
 pub mod kmeans;
 pub mod lvq;
 pub mod params;
 pub mod points;
-
-#[cfg(test)]
-mod tests {
-
-    use crate::hnsw::{index::HNSW, points::Point};
-    use rand::Rng;
-
-    use super::params::Params;
-
-    #[test]
-    fn hnsw_construction() {
-        let params = Params::from_m(12, 100);
-        let _index: HNSW = HNSW::new(params.m, None, params.dim);
-        let _index: HNSW = HNSW::from_params(params);
-    }
-
-    #[test]
-    fn hnsw_insert() {
-        let mut rng = rand::thread_rng();
-        let dim = 100;
-        let mut index: HNSW = HNSW::new(12, None, dim);
-        let n: usize = 100;
-
-        for i in 0..n {
-            let vector = (0..dim).map(|_| rng.gen::<f32>()).collect();
-            let point = Point::new(i, vector, false);
-            let mut level = 0;
-            if rng.gen::<f32>() > 0.5 {
-                level = 1;
-            }
-            index.insert(point, level);
-        }
-
-        let already_in_index = 0;
-        let vector = (0..dim).map(|_| rng.gen::<f32>()).collect();
-        let point = Point::new(already_in_index, vector, false);
-        index.insert(point, 0);
-        assert_eq!(index.points.len(), n);
-    }
-
-    #[test]
-    fn hnsw_serialize() -> std::io::Result<()> {
-        let mut rng = rand::thread_rng();
-        let dim = 100;
-        let mut index: HNSW = HNSW::new(12, None, dim);
-        let n: usize = 100;
-
-        for i in 0..n {
-            let vector = (0..dim).map(|_| rng.gen::<f32>()).collect();
-            let point = Point::new(i, vector, true);
-            let mut level = 0;
-            if rng.gen::<f32>() > 0.5 {
-                level = 1;
-            }
-            index.insert(point, level);
-        }
-        let index_path = "./hnsw_index.json";
-        index.save(index_path)?;
-        let loaded_index = HNSW::from_path(index_path)?;
-        assert_eq!(n, loaded_index.points.len());
-        std::fs::remove_file(index_path)?;
-        Ok(())
-    }
-}
---
+++ hnsw/tests/hnsw_tests.rs
@@ -0,0 +1,62 @@
+extern crate hnsw;
+use core::panic;
+
+use hnsw::helpers::glove::load_glove_array;
+use hnsw::hnsw::index::HNSW;
+use hnsw::hnsw::params::Params;
+use rand::Rng;
+
+const DIM: usize = 100;
+const N: usize = 100;
+
+#[test]
+fn hnsw_init() {
+    let params = Params::from_m(12, DIM);
+    let _index: HNSW = HNSW::new(params.m, None, params.dim);
+    let _index: HNSW = HNSW::from_params(params);
+}
+
+#[test]
+fn hnsw_build() {
+    let vectors = make_rand_vectors(N);
+    let mut index = HNSW::build_index(12, None, vectors, false).unwrap();
+    index.insert(0, 0).unwrap();
+
+    assert_eq!(index.points.len(), N);
+}
+
+#[test]
+fn hnsw_build_small_loop() {
+    let (_, vectors) = load_glove_array(DIM, 10, true, 1).expect("Could not load glove");
+    for _ in 0..100_000 {
+        let _ = HNSW::build_index(12, None, vectors.clone(), false).unwrap();
+    }
+}
+
+#[test]
+fn hnsw_build_big() {
+    let (_, vectors) = load_glove_array(DIM, 40_000, true, 1).expect("Could not load glove");
+    let _ = HNSW::build_index(24, None, vectors, false).unwrap();
+}
+
+#[test]
+fn hnsw_serialize() {
+    let index = HNSW::build_index(24, None, make_rand_vectors(N), false).unwrap();
+
+    let index_path = "./hnsw_index.json";
+    index.save(index_path).unwrap();
+    let loaded_index = HNSW::from_path(index_path).unwrap();
+
+    assert_eq!(N, loaded_index.points.len());
+    std::fs::remove_file(index_path).unwrap();
+}
+
+fn make_rand_vectors(n: usize) -> Vec<Vec<f32>> {
+    let mut rng = rand::thread_rng();
+    let mut vectors = Vec::new();
+    for _ in 0..n {
+        let vector = (0..DIM).map(|_| rng.gen::<f32>()).collect();
+        vectors.push(vector)
+    }
+    vectors
+}
