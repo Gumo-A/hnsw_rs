580b8e62aedd13e699a0edd31af064638ca4b553
Gamal Abdulsalam <gamalalejandro@gmail.com>
2024-08-12 10:15:40 +0200

    in points.rs. Separated "new" function into quantized and full functions
    Added some functions

--- hnsw/src/hnsw/points.rs
+++ hnsw/src/hnsw/points.rs
@@ -62,51 +62,56 @@ impl Vector {
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Point {
     pub id: usize,
+    pub level: Option<usize>,
     pub vector: Vector,
 }
 
 impl Point {
-    pub fn new(id: usize, vector: Vec<f32>, quantize: bool) -> Point {
-        let vector_stored = if quantize {
-            Vector::Compressed(LVQVec::new(&vector, 8))
-        } else {
-            Vector::Full(vector)
-        };
+    pub fn new_full(id: usize, level: Option<usize>, vector: Vec<f32>) -> Point {
         Point {
             id,
-            vector: vector_stored,
+            level,
+            vector: Vector::Full(vector),
+        }
+    }
+
+    pub fn new_quantized(id: usize, level: Option<usize>, vector: &Vec<f32>) -> Point {
+        Point {
+            id,
+            level,
+            vector: Vector::Compressed(LVQVec::new(vector, 8)),
         }
     }
 
     pub fn from_vector(id: usize, vector: Vector) -> Point {
-        Point { id, vector }
+        Point {
+            id,
+            level: None,
+            vector,
+        }
+    }
+
+    pub fn dist2other(&self, other: &Point) -> Dist {
+        self.dist2vec(&other.vector)
     }
 
     pub fn dist2vec(&self, other_vec: &Vector) -> Dist {
         let dist = match &self.vector {
             Vector::Compressed(compressed_self) => match other_vec {
                 Vector::Compressed(compressed_other) => {
-                    // println!("Q 2 Q");
                     compressed_self.dist2other(&compressed_other)
                 }
-                Vector::Full(full_other) => {
-                    // println!("Q 2 F");
-                    compressed_self.dist2vec(&full_other)
-                }
+                Vector::Full(full_other) => compressed_self.dist2vec(&full_other),
             },
             Vector::Full(full_self) => match other_vec {
-                Vector::Compressed(compressed_other) => {
-                    // println!("F 2 Q");
-                    compressed_other.dist2vec(&full_self)
-                }
-                Vector::Full(full_other) => {
-                    // println!("F 2 F");
-                    let mut result = 0.0;
-                    for (x, y) in full_self.iter().zip(full_other) {
-                        result += (x - y).powi(2);
-                    }
-                    Dist::new(result.sqrt())
-                }
+                Vector::Compressed(compressed_other) => compressed_other.dist2vec(&full_self),
+                Vector::Full(full_other) => Dist::new(
+                    full_self
+                        .iter()
+                        .zip(full_other.iter())
+                        .fold(0.0, |acc, e| acc + (e.0 - e.1).powi(2))
+                        .sqrt(),
+                ),
             },
         };
         dist
@@ -146,6 +151,18 @@ impl Points {
             Points::Collection(points) => points.keys(),
         }
     }
+
+    pub fn ids_levels(&self) -> impl Iterator<Item = (usize, usize)> + '_ {
+        match self {
+            Points::Empty => {
+                panic!("Tried to get ids, but there are no stored vectors in the index.");
+            }
+            Points::Collection(points) => points
+                .values()
+                .map(|point| (point.id, point.level.unwrap())),
+        }
+    }
+
     pub fn insert(&mut self, point: Point) {
         match self {
             Points::Empty => {
@@ -177,13 +194,14 @@ impl Points {
         }
     }
 
-    pub fn pop(&mut self, index: &usize) -> Option<Point> {
+    pub fn pop(&mut self, index: usize) -> Option<Point> {
         match self {
             Points::Empty => None,
-            Points::Collection(points) => points.remove(index),
+            Points::Collection(points) => points.remove(&index),
         }
     }
 
+    /// Removes all the points with the ids and returns them in a new Points struct.
     pub fn pop_multiple(&mut self, ids: &Vec<usize>) -> Option<Self> {
         match self {
             Points::Empty => None,
@@ -207,19 +225,15 @@ impl Points {
         }
     }
 
-    pub fn get_point(&self, index: usize) -> &Point {
-        let point: &Point = match self {
+    pub fn get_point(&self, index: usize) -> Option<&Point> {
+        match self {
             Points::Empty => {
                 panic!(
                     "Tried to get point with index {index}, but there are no stored vectors in the index."
                 );
             }
-            Points::Collection(points) => points
-                .get(&index)
-                .expect(format!("Could not get point {index} from collection.").as_str()),
-        };
-
-        point
+            Points::Collection(points) => points.get(&index),
+        }
     }
 
     pub fn get_points(&self, indices: &HashSet<usize, BuildNoHashHasher<usize>>) -> Vec<&Point> {
@@ -248,24 +262,29 @@ impl Points {
 
     /// Extends the Collection variant with the provided vector,
     /// or, in the case it is the Empty variant, fills it and changes the variant to Collection.
-    pub fn extend_or_fill(&mut self, points: Vec<Point>) {
+    pub fn extend_or_fill(&mut self, other: Points) {
+        let points = match other {
+            Self::Empty => return (),
+            Self::Collection(points_map) => points_map,
+        };
+
         match self {
             Points::Empty => {
-                let mut collection =
-                    HashMap::with_capacity_and_hasher(points.len(), BuildNoHashHasher::default());
-                let mut idx = 0;
-                for point in points {
-                    collection.insert(idx, point);
-                    idx += 1;
-                }
-                *self = Points::Collection(collection);
+                *self = Points::Collection(points);
             }
             Points::Collection(points_map) => {
-                let old_max = points_map.keys().max().unwrap_or(&0) + 1;
-                let mut idx = 1;
-                for point in points {
-                    points_map.insert(old_max + idx, point);
-                    idx += 1;
+                let old_ids: HashSet<usize> = points_map.keys().cloned().collect();
+                let new_ids: HashSet<usize> = points.keys().cloned().collect();
+                let intersection_len = new_ids.intersection(&old_ids).count();
+
+                if intersection_len > 0 {
+                    println!("At least one id in the new points are present in the old ids.");
+                    println!("Cannot insert the new points to the collection.");
+                    return ();
+                }
+
+                for (id, point) in points {
+                    points_map.insert(id, point);
                 }
             }
         }
